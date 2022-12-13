f8ef4d64f1083040574e6b2e78d3a8f26141e750
storres20 <sanchotorres777@gmail.com>
2022-12-13 11:35:33 -0500

    Add Redux to change status

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkingStatus } from '../redux/categories/categories';
 
 function Categories() {
+  /* Setup Redux dispatch */
+  const dispatch = useDispatch();
+
+  /* After clicking on "Check Status" button */
+  const handleStatus = () => {
+    dispatch(checkingStatus());
+  };
+
+  /* Load Redux State */
+  const status = useSelector((state) => state.status);
+
   return (
     <div>
-      <button type="button">Check status</button>
+      <button type="button" onClick={handleStatus}>Check status</button>
+      <h2>{status}</h2>
     </div>
   );
 }
