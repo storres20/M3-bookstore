7d29f7712fee6b397158f9f8daf11353d6984624
storres20 <sanchotorres777@gmail.com>
2022-12-14 02:01:27 -0500

    Add styles and import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+import { BsPersonFill } from 'react-icons/bs';
 
 function Navbar() {
   return (
-    <div>
-      <NavLink to="/">BOOKS</NavLink>
-      <NavLink to="/categories">CATEGORIES</NavLink>
+    <div className="navContainer">
+      <div className="navLinks">
+        <NavLink to="/" className="brand">Bookstore CMS</NavLink>
+        <NavLink to="/" className="navLink">BOOKS</NavLink>
+        <NavLink to="/categories" className="navLink">CATEGORIES</NavLink>
+      </div>
+      <button type="button" className="icon-button">
+        <BsPersonFill className="color" />
+      </button>
+
     </div>
   );
 }
